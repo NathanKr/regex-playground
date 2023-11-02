8d9054b65dbcc3db867643f56e07e47c7606b1d2
Nathan Krasney <natankrasney@gmail.com>
2023-11-02 13:04:13 +0200

    regex with or

--- test/regex.test.ts
+++ test/regex.test.ts
@@ -16,3 +16,14 @@ test('does israel appear in text - use RegEx constructor',()=>{
     const israelInPattern = regexPttern.test(text)
     expect(israelInPattern).toBeTruthy()
 })
+
+test('match with or : israel or jesrusalem appear',()=>{
+    const regexPttern = /israel|jerusalem/i; // literal notation
+    const text1 = 'does Israel appear in text - case not sensitive';
+    expect(regexPttern.test(text1)).toBeTruthy()
+    const text2 = 'does Jerusalem appear in text - case not sensitive';
+    expect(regexPttern.test(text2)).toBeTruthy()
+    const text3 = 'does TelAviv appear in text - case not sensitive';
+    expect(regexPttern.test(text3)).toBeFalsy()
+
+})
